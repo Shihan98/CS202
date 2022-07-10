77e3d1c601f260619d95445cddacb4695ed3d84f
shihan98 <shihanthathsara98@gmail.com>
2022-07-10 22:29:46 +0530

    tree

--- BST/src/main/java/Stack.java
+++ BST/src/main/java/Stack.java
@@ -15,6 +15,30 @@ public class Stack {
 
     public void push(TreeNode node){
         StackNode newNode = new StackNode(node);
+        if (isEmptyS()){
+            this.top = newNode;
+        }else {
+            this.top.next = newNode;
+            this.top = newNode;
+        }
+    }
 
+    public TreeNode pop(){
+        if (isEmptyS()){
+            return null;
+        }else {
+            StackNode temp = this.top;
+            this.top = temp.next;
+            temp.next = null;
+            return  temp.node;
+        }
+    }
+
+    public TreeNode peek(){
+        if (isEmptyS()){
+            return null;
+        }else {
+            return this.top.node;
+        }
     }
 }
